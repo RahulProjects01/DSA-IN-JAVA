d39620d2ec57ac1a42d89e1f45bc417f8395ffd4
RahulProjects01 <srahulb6@gmail.com>
2023-12-24 16:26:13 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+public static void printFibonacci(int n){
+    int a = 0;
+    int b = 1;
+    int c= 1;
+
+    for(int i=1;i<=n;i++){
+        System.out.print(a+", ");
+        a= b;
+        b=c;
+        c = a+b;
+    }
+}
+
+    public static void main(String[] args) {
+     printFibonacci(10);
+    }
+}
