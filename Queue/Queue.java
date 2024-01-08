417571feae7f77b2ddebd4bfca00617e14f3a33d
RahulProjects01 <srahulb6@gmail.com>
2024-01-08 10:47:20 +0530

    java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -95,3 +95,86 @@
 // q.display();
 //     }
 // }
+
+//LinkedList node through Queue
+public class Queue {
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static class QueueA {
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+
+        public void add(int x) {
+            Node temp = new Node(x);
+            if (size == 0) {
+                head = tail = temp;
+
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public int peek() {
+            if (size == 0) {
+                System.out.println("Queue empty");
+                return -1;
+            }
+            return head.val;
+        }
+
+        public int remove() {
+            if (size == 0) {
+                System.out.println("queue empty");
+                return -1;
+            }
+            int x = head.val;
+            head = head.next;
+            size--;
+            return x;
+        }
+
+        public void display() {
+            if (size == 0) {
+                System.out.println("queue is empty");
+            }
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        QueueA q = new QueueA();
+        q.display();
+        q.add(23);
+        q.add(43);
+        q.add(43);
+        q.add(44);
+        System.out.println("print all element");
+        q.display();
+        System.out.println("remove");
+        q.remove();
+        q.display();
+        System.out.println("peek element");
+        System.out.println(q.peek());
+
+    }
+}
+
+
+
+
+ 
---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,42 @@
+// public class BubbleSort {
+//     public static void main(String[] args) {
+//         int a[] = { 1, 5, 3, 4, 7, 2, 9 };
+
+//         for (int i = 0; i < a.length; i++) {
+//             for (int j = 0; j < a.length - i - 1; j++) {
+//                 if (a[j] > a[j + 1]) {
+//                     int temp = a[j];
+//                     a[j] = a[j + 1];
+//                     a[j + 1] = temp;
+//                 }
+//             }
+//         }
+
+//         for (int i = 0; i < a.length; i++) {
+//             System.out.print(a[i]+" ");
+//         }
+//     }
+// }
+// ===================================================================================
+
+// public class BubbleSort{
+//     public static void main(String[] args) {
+//         int a[] = { 1, 3, 5, 7, 2, 3, 4 };
+//         for (int i = 0; i < a.length; i++) {
+//             int j = i;
+//             while (j > 0 && a[j] < a[j - 1]) {
+//                 int temp = a[j];
+//                 a[j] = a[j - 1];
+//                 a[j - 1] = temp;
+//                 j--;
+//             }
+//         }
+        
+//         for (int i = 0; i < a.length; i++) {
+//             System.out.print(a[i]+" ");
+//         }
+//     }
+// }
+
+//=====================================================================================
+
